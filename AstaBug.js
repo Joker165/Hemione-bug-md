f9c58cb90f4cfef07f3dd3d6ba472bd3ab9d17c1
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-11-05 13:35:31 +0100

    Update AstaBug.js

--- AstaBug.js
+++ AstaBug.js
@@ -1196,6 +1196,23 @@ break
                 }
             }
             break
+			case 'vv': {
+	if (!m.quoted) return replygcxeon(`Reply to view once message`)
+	if (m.quoted.mtype !== 'viewOnceMessageV2') return replygcxeon(`This is not a view once message`)
+    let msg = m.quoted.message
+    let type = Object.keys(msg)[0]
+    let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+    let buffer = Buffer.from([])
+    for await (const chunk of media) {
+        buffer = Buffer.concat([buffer, chunk])
+    }
+    if (/video/.test(type)) {
+        return XeonBotInc.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+    } else if (/image/.test(type)) {
+        return XeonBotInc.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+    }
+}
+break
             case 'toqr': {
                 if (!q) return replygcxeon(' Please include link or text!')
                 const qrcode = require('qrcode')
